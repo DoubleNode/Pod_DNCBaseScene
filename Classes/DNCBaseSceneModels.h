bb8b1b4161d8b5f8186651b3263329712e049dac
Darren Ehlers <me@darrenehlers.com>
2017-02-07 14:25:16 -0600

    Changed userData retain to strong and updated podspec version

--- Classes/DNCBaseSceneModels.h
+++ Classes/DNCBaseSceneModels.h
@@ -56,7 +56,7 @@
 @property (assign, atomic)  UIAlertActionStyle      button1Style;
 @property (assign, atomic)  UIAlertActionStyle      button2Style;
 
-@property (copy, nonatomic) id  userData;
+@property (strong, nonatomic) id  userData;
 
 @end
 
@@ -115,7 +115,7 @@
 @property (assign, atomic)  UIAlertActionStyle      button1Style;
 @property (assign, atomic)  UIAlertActionStyle      button2Style;
 
-@property (copy, nonatomic) id  userData;
+@property (strong, nonatomic) id  userData;
 
 @end
 
--- DNCBaseScene.podspec
+++ DNCBaseScene.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "DNCBaseScene"
-  s.version      = "0.0.9"
+  s.version      = "0.0.10"
   s.summary      = "A base class for scenes."
 
   # This description is used to generate tags and improve search results.
